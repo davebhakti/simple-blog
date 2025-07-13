a8101f7bd112d80253b38682dec7bb5fb8103e19
davebhakti <bhaktidave2206@gmail.com>
2025-07-12 19:20:24 -0700

    COMPLETED

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ let  nextID = 1;
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(__dirname + '../public'));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.render('index', {posts: posts});
